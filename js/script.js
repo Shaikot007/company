e0963604905f5f9965f953461330b8fc350a7f4c
Shaikot007 <shaikot999@gmail.com>
2022-04-02 22:03:40 +0600

    Create team

--- js/script.js
+++ js/script.js
@@ -9,4 +9,13 @@ function myFunction() {
     video.pause();
     btn.innerHTML = "<img class='video_button' src='./assests/video/play.png' alt='Play' />";
   }
-}
+};
+
+$(document).ready(function() {
+  $(".business-service").mouseover(function() {
+    $(".card-triangle-bottomright").css("border-bottom", "100px solid #FF4917");
+  });
+  $(".business-service").mouseleave(function() {
+    $(".card-triangle-bottomright").css("border-bottom", "100px solid rgba(255, 73, 23, 0.2)");
+  });
+});
